d8f172cda66beb17f4e0d7dca87eee8f675690fa
Joachim Hummel <joachim.hummel@gmail.com>
2017-02-09 20:29:09 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 
 // reply to request with "Hello World!"
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hallo München!');
 });
 
 //start a server on port 80 and log its start to our console
